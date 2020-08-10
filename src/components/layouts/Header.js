cb8e16c3886d3f311abbd2ce2aabd9303d16b900
Ucheduk <dukweb4@gmail.com>
2020-08-10 15:35:35 +0100

    initial commit

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <PersonIcon />
+      <ForumIcon />
+    </div>
+  )
+}
+
+export default Header
